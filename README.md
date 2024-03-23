9e511fceb79128f43004ce9a12501434f71ac214
Barbara <subwaynaas@yahoo.ie>
2024-03-23 17:58:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+As always, I learned a lot and struggled even more. FEM really does love those background images, don't they? I felt position: absolute was neccessary here for the avatar and buttons but it does break my heart as I try to get it to be responsive.
+
+Three things that I couldn't figure out:
+
+The position of the person/student is not printing on screen when I click the btns - it seems to be something with the .textContent as I have done the console.logs up to that bit and all works fine.
+
+The max-width in my media query for the container is not working - that has never not worked for me before so I can't figure out what I am doing wron this time.
+
+The box shadow under the button container is only on one side and should be on both. Maybe I should just do a box shadow on an invisible line under the image instead?
---
+++ app.js
@@ -0,0 +1,66 @@
+const testimonials = [
+  {
+    id: 1,
+    student: "Tanya Sinclair",
+    position: "UX Engineer",
+    quote:
+      "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”",
+    img: "images/image-tanya.jpg",
+  },
+  {
+    id: 2,
+    student: "John Tarkpor",
+    position: "Junior Front-end Developer",
+    quote:
+      "“ If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+    img: "images/image-john.jpg",
+  },
+  {
+    id: 3,
+    student: "Barbara Ward",
+    position: "Front-end Developer with LOTS to learn",
+    quote:
+      "“ I did NOT take this coding course but I wanted to add another item to click on. So this is a photo of me - I am too big but I didn't want the hassle of resizing it. Happy Coding everyone! ”",
+    img: "images/image-barbara.jpg",
+  },
+];
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+const contentContainer = document.querySelector(".content-container");
+const avatar = document.querySelector(".avatar");
+const quote = contentContainer.querySelector(".quote");
+const student = contentContainer.querySelector(".student");
+const position = contentContainer.querySelector(".position");
+console.log(position);
+
+let currentItem = 0;
+
+const showPerson = () => {
+  let item = testimonials[currentItem];
+
+  avatar.src = item.img;
+
+  student.textContent = item.student;
+  position.textContent = item.position;
+  quote.textContent = item.quote;
+
+  console.log(item.position);
+};
+
+nextBtn.addEventListener("click", () => {
+  currentItem++;
+  if (currentItem > testimonials.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+prevBtn.addEventListener("click", () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = testimonials.length - 1;
+  }
+  showPerson();
+});
